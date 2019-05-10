981eb39fe886b25f522497e38fda7e9704f6938a
Arnold Alejo Nunag <arnoldnunag12@gmail.com>
2019-05-11 00:25:41 +0800

    Remove unused method

--- src/main/java/sciwhiz12/voxeltools/VoxelTools.java
+++ src/main/java/sciwhiz12/voxeltools/VoxelTools.java
@@ -4,12 +4,10 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import net.minecraft.item.Item;
-import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import sciwhiz12.voxeltools.item.VxItems;
@@ -22,18 +20,12 @@ public class VoxelTools {
 
 	public VoxelTools() {
 		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setupClient);
 	}
 
 	private void setup(final FMLCommonSetupEvent event) {
 		LOGGER.info("Setting up common...");
 	}
-
-	private void setupClient(final FMLClientSetupEvent event) {
-		// do something that can only be done on the client
-		LOGGER.info("Setting up client...");
-	}
-
+	
 	@Mod.EventBusSubscriber(bus = Bus.MOD)
 	public static class RegistryEvents {
 		@SubscribeEvent
