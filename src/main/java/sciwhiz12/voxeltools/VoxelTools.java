a84378b3fba61f29631b3b4b33c65e9680e78665
Arnold Alejo Nunag <arnoldnunag12@gmail.com>
2019-05-13 20:25:06 +0800

    Added 'defaultPermLevel' config option

--- src/main/java/sciwhiz12/voxeltools/VoxelTools.java
+++ src/main/java/sciwhiz12/voxeltools/VoxelTools.java
@@ -4,6 +4,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import net.minecraft.item.Item;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.ModLoadingContext;
@@ -11,8 +12,8 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.server.FMLServerAboutToStartEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.server.permission.DefaultPermissionLevel;
 import net.minecraftforge.server.permission.PermissionAPI;
 import sciwhiz12.voxeltools.item.VxItems;
 
@@ -29,7 +30,12 @@ public class VoxelTools {
 
 	private void setup(final FMLCommonSetupEvent event) {
 		LOGGER.info("Setting up common...");
-		PermissionAPI.registerNode(VxConfig.ITEM_USE_PERMISSION, DefaultPermissionLevel.OP,
+		MinecraftForge.EVENT_BUS.register(this);
+	}
+	
+	@SubscribeEvent
+	public void handleServerAboutToStartEvent(final FMLServerAboutToStartEvent event) {
+		PermissionAPI.registerNode(VxConfig.ITEM_USE_PERMISSION, VxConfig.SERVER.defaultPermLevel.get(),
 				"Allows the use of VoxelTools.");
 	}
 
--- src/main/java/sciwhiz12/voxeltools/VxConfig.java
+++ src/main/java/sciwhiz12/voxeltools/VxConfig.java
@@ -7,6 +7,7 @@ import net.minecraftforge.common.ForgeConfigSpec;
 import net.minecraftforge.common.ForgeConfigSpec.BooleanValue;
 import net.minecraftforge.common.ForgeConfigSpec.EnumValue;
 import net.minecraftforge.common.ForgeConfigSpec.IntValue;
+import net.minecraftforge.server.permission.DefaultPermissionLevel;
 import net.minecraftforge.server.permission.PermissionAPI;
 
 public final class VxConfig {
@@ -20,14 +21,20 @@ public final class VxConfig {
 
 	public static class Server {
 		public final BooleanValue allowItemUse;
+		public final EnumValue<DefaultPermissionLevel> defaultPermLevel;
 		public final IntValue paintbrushRange;
 		public final EnumValue<ClickType> jackhammerNoPhys;
+		public final BooleanValue allowOverwrite;
 
 		public Server(ForgeConfigSpec.Builder builder) {
 			builder.comment("General settings").push("general");
 
 			allowItemUse = builder.translation("voxeltools.config.allowItemUse")
 					.comment("Enables/disables the functionality of all items.").define("allowItemUse", true);
+
+			defaultPermLevel = builder.translation("voxeltools.config.defaultPermLevel").comment(
+					"The default permission level of the permission [voxeltools.item], which grants players access to VoxelTools.")
+					.defineEnum("defaultPermLevel", DefaultPermissionLevel.OP, DefaultPermissionLevel.values());
 
 			paintbrushRange = builder.translation("voxeltools.config.paintbrushRange").comment(
 					"The range of the Magical Paintbrush when remotely painting a block. Anything below the player's reach distance (default 5) will disable remote painting.")
@@ -36,6 +43,11 @@ public final class VxConfig {
 			jackhammerNoPhys = builder.translation("voxeltools.config.jackhammerNoPhys")
 					.comment("Whether to use no-physics mode for either mouse button, both, or neither.")
 					.defineEnum("jackhammerNoPhys", ClickType.RIGHT_CLICK_ONLY, ClickType.values());
+
+			allowOverwrite = builder.translation("voxeltools.config.allowOverwrite").comment(
+					"Whether to allow block-moving items (ie Graviton Sledge) to destroy blocks in their way when the player is sneaking.")
+					.define("allowOverwrite", true);
+
 			builder.pop();
 		}
 
