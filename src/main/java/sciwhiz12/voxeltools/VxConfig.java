ef827b134f623bf5abc8a8828b0c37249d90b457
Arnold Alejo Nunag <arnoldnunag12@gmail.com>
2019-05-13 10:13:45 +0800

    Add 'jackhammerNoPhys' config option

--- src/main/java/sciwhiz12/voxeltools/VxConfig.java
+++ src/main/java/sciwhiz12/voxeltools/VxConfig.java
@@ -4,6 +4,7 @@ import org.apache.commons.lang3.tuple.Pair;
 
 import net.minecraftforge.common.ForgeConfigSpec;
 import net.minecraftforge.common.ForgeConfigSpec.BooleanValue;
+import net.minecraftforge.common.ForgeConfigSpec.EnumValue;
 import net.minecraftforge.common.ForgeConfigSpec.IntValue;
 
 public final class VxConfig {
@@ -18,6 +19,7 @@ public final class VxConfig {
 	public static class Server {
 		public final BooleanValue allowItemUse;
 		public final IntValue paintbrushRange;
+		public final EnumValue<ClickType> jackhammerNoPhys;
 
 		public Server(ForgeConfigSpec.Builder builder) {
 			builder.comment("General settings").push("general");
@@ -29,7 +31,15 @@ public final class VxConfig {
 					"The range of the Magical Paintbrush when remotely painting a block. Anything below the player's reach distance (default 5) will disable remote painting.")
 					.defineInRange("paintbrushRange", 16, 0, 48);
 
+			jackhammerNoPhys = builder.translation("voxeltools.config.jackhammerNoPhys")
+					.comment("Whether to use no-physics mode for either mouse button, both, or neither.")
+					.defineEnum("jackhammerNoPhys", ClickType.RIGHT_CLICK_ONLY, ClickType.values());
+
 			builder.pop();
 		}
+	}
+
+	public static enum ClickType {
+		BOTH, RIGHT_CLICK_ONLY, LEFT_CLICK_ONLY, NONE;
 	}
 }
--- src/main/java/sciwhiz12/voxeltools/item/Jackhammer.java
+++ src/main/java/sciwhiz12/voxeltools/item/Jackhammer.java
@@ -9,6 +9,7 @@ import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.eventbus.api.Event.Result;
 import sciwhiz12.voxeltools.VxConfig;
+import sciwhiz12.voxeltools.VxConfig.ClickType;
 
 public class Jackhammer extends BaseItem {
 
@@ -20,7 +21,11 @@ public class Jackhammer extends BaseItem {
 	public Result onLeftClickBlock(EntityPlayer player, BlockPos pos, EnumFacing face) {
 		if (VxConfig.SERVER.allowItemUse.get() && !player.world.isRemote) {
 			player.world.playEvent(2001, pos, Block.getStateId(player.world.getBlockState(pos)));
-			player.world.setBlockState(pos, Blocks.AIR.getDefaultState(), 2 | 16 | 32);
+			int flags = 2;
+			ClickType option = VxConfig.SERVER.jackhammerNoPhys.get();
+			if (option == ClickType.LEFT_CLICK_ONLY || option == ClickType.BOTH)
+				flags |= 16 | 32;
+			player.world.setBlockState(pos, Blocks.AIR.getDefaultState(), flags);
 			return Result.DENY;
 		}
 		return Result.DEFAULT;
@@ -29,8 +34,13 @@ public class Jackhammer extends BaseItem {
 	@Override
 	public EnumActionResult onItemUse(ItemUseContext context) {
 		if (VxConfig.SERVER.allowItemUse.get() && !context.getWorld().isRemote) {
-			context.getWorld().playEvent(2001, context.getPos(), Block.getStateId(context.getWorld().getBlockState(context.getPos())));
-			context.getWorld().setBlockState(context.getPos(), Blocks.AIR.getDefaultState(), 2 | 16 | 32);
+			context.getWorld().playEvent(2001, context.getPos(),
+					Block.getStateId(context.getWorld().getBlockState(context.getPos())));
+			int flags = 2;
+			ClickType option = VxConfig.SERVER.jackhammerNoPhys.get();
+			if (option == ClickType.RIGHT_CLICK_ONLY || option == ClickType.BOTH)
+				flags |= 16 | 32;
+			context.getWorld().setBlockState(context.getPos(), Blocks.AIR.getDefaultState(), flags);
 			return EnumActionResult.SUCCESS;
 		}
 		return EnumActionResult.PASS;
