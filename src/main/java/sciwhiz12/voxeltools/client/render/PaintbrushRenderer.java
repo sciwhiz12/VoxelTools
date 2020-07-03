ce644ad9dd0a2ecfca74fdb53af57979e6821a66
Arnold Alejo Nunag <arnoldnunag12@gmail.com>
2020-07-03 17:04:37 +0800

    Fix lighting of block render in paintbrush

--- src/main/java/sciwhiz12/voxeltools/client/render/PaintbrushRenderer.java
+++ src/main/java/sciwhiz12/voxeltools/client/render/PaintbrushRenderer.java
@@ -5,10 +5,7 @@ import com.mojang.blaze3d.vertex.IVertexBuilder;
 import net.minecraft.block.BlockState;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.client.renderer.IRenderTypeBuffer;
-import net.minecraft.client.renderer.ItemRenderer;
-import net.minecraft.client.renderer.RenderTypeLookup;
-import net.minecraft.client.renderer.Vector3f;
+import net.minecraft.client.renderer.*;
 import net.minecraft.client.renderer.model.IBakedModel;
 import net.minecraft.client.renderer.model.ItemCameraTransforms.TransformType;
 import net.minecraft.client.renderer.tileentity.ItemStackTileEntityRenderer;
@@ -27,39 +24,42 @@ public class PaintbrushRenderer extends ItemStackTileEntityRenderer {
             int combinedOverlay) {
         final Minecraft mc = Minecraft.getInstance();
         final ItemRenderer itemRenderer = mc.getItemRenderer();
-        boolean renderingBlockFull = false;
+        boolean renderItem = true;
         IBakedModel brushModel = itemRenderer.getItemModelWithOverrides(itemStack, null, null);
         if (brushModel instanceof ISTERWrapper) {
             if (((ISTERWrapper) brushModel).getCurrentPerspective() == TransformType.GUI) {
                 CompoundNBT tag = itemStack.getChildTag(Paintbrush.TAG_ID_STOREDBLOCK);
                 if (tag != null) {
-                    BlockState state = NBTUtil.readBlockState(tag);
-                    IBakedModel model = mc.getBlockRendererDispatcher().getModelForState(state);
-                    IVertexBuilder vertex = buffer.getBuffer(RenderTypeLookup.getRenderType(state));
                     matrixStack.push();
                     matrixStack.rotate(Vector3f.XP.rotationDegrees(30));
                     matrixStack.rotate(Vector3f.YP.rotationDegrees(225));
                     if (Screen.hasShiftDown()) {
-                        renderingBlockFull = true;
+                        renderItem = false;
                         matrixStack.translate(-0.66D, 0.25D, -0.66D);
                         matrixStack.scale(0.625F, 0.625F, 0.625F);
                     } else {
                         matrixStack.scale(0.35F, 0.35F, 0.35F);
                         matrixStack.translate(-2D, 0.5D, -2D);
                     }
-                    Minecraft.getInstance().getBlockRendererDispatcher().getBlockModelRenderer()
-                            .renderModel(matrixStack.getLast(), vertex, state, model, 0.F, 0.F, 0.F, combinedLight,
-                                    combinedOverlay, EmptyModelData.INSTANCE);
+                    RenderHelper.setupGui3DDiffuseLighting();
+                    BlockState state = NBTUtil.readBlockState(tag);
+                    IRenderTypeBuffer.Impl specialBuffer = Minecraft.getInstance().getRenderTypeBuffers().getBufferSource();
+                    Minecraft.getInstance().getBlockRendererDispatcher()
+                            .renderBlock(state, matrixStack, specialBuffer, combinedLight, combinedOverlay,
+                                    EmptyModelData.INSTANCE);
+                    specialBuffer.finish();
+                    RenderHelper.setupGuiFlatDiffuseLighting();
                     matrixStack.pop();
                 }
             }
         }
-        if (!renderingBlockFull) {
+        if (renderItem) {
             matrixStack.push();
             IVertexBuilder builder = ItemRenderer
                     .getBuffer(buffer, RenderTypeLookup.getRenderType(itemStack), true, itemStack.hasEffect());
-            itemRenderer.renderQuads(matrixStack, builder, brushModel.getQuads(null, null, new Random(42L)), itemStack,
-                    combinedLight, combinedOverlay);
+            itemRenderer.renderQuads(matrixStack, builder,
+                    brushModel.getQuads(null, null, new Random(42L), EmptyModelData.INSTANCE), itemStack, combinedLight,
+                    combinedOverlay);
             matrixStack.pop();
         }
     }
