3ff66d4d0b67bd193e52258d517b4c2649366a33
Arnold Alejo Nunag <arnoldnunag12@gmail.com>
2020-08-03 04:47:17 +0800

    Fix clock texture not updating when in inventory

--- src/main/java/sciwhiz12/voxeltools/client/ClientSetup.java
+++ src/main/java/sciwhiz12/voxeltools/client/ClientSetup.java
@@ -1,6 +1,8 @@
 package sciwhiz12.voxeltools.client;
 
+import net.minecraft.client.world.ClientWorld;
 import net.minecraft.entity.Entity;
+import net.minecraft.item.IItemPropertyGetter;
 import net.minecraft.item.ItemModelsProperties;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
@@ -15,20 +17,23 @@ import sciwhiz12.voxeltools.item.VxItems;
 @EventBusSubscriber(value = Dist.CLIENT, modid = VoxelTools.MODID, bus = Bus.MOD)
 public class ClientSetup {
 
+    public static final IItemPropertyGetter CLOCK_PROPERTY = (stack, clientWorld, livingEntity) -> {
+        double value = 0F;
+        Entity entity = livingEntity != null ? livingEntity : stack.getAttachedEntity();
+        if (entity != null) {
+            if (clientWorld == null && entity.world instanceof ClientWorld) {
+                clientWorld = (ClientWorld) entity.world;
+            }
+            if (clientWorld != null) {
+                value = clientWorld.func_230315_m_().func_236032_b_(stack.getOrCreateTag().getLong(Clock.TAG_FIXED_TIME));
+            }
+        }
+        return (float) value;
+    };
+
     @SubscribeEvent
     static void onClientSetup(FMLClientSetupEvent event) {
         VoxelTools.LOGGER.debug("Setting up client...");
-        ItemModelsProperties
-                .func_239418_a_(VxItems.clock.get(), new ResourceLocation("time"), (stack, world, livingEntity) -> {
-                    Entity entity = livingEntity != null ? livingEntity : stack.getItemFrame();
-                    double value = 0.0F;
-                    if (world != null) {
-                        if (world.func_230315_m_().func_236043_f_()) {
-                            value = world.func_230315_m_()
-                                    .func_236032_b_(stack.getOrCreateTag().getLong(Clock.TAG_FIXED_TIME));
-                        }
-                    }
-                    return (float) value;
-                });
+        ItemModelsProperties.func_239418_a_(VxItems.clock.get(), new ResourceLocation("time"), CLOCK_PROPERTY);
     }
 }
